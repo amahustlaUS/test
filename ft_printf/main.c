176302a7d33741e0ae02f96fc907db0e3de6098d
amahustlaus <amahustlaus@gmail.com>
2019-01-18 00:00:40 +0200

    instabot

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -7,8 +7,8 @@ int main()
 
  // x = ft_printf("0123456789");
 
- 	   x1 = printf("O123% 0+123d\n", -420);
-	x2 = ft_printf("M123% 0+123d\n", -420);
+ 	   x1 = printf("O123%+0123d\n", -420);
+	x2 = ft_printf("M123%+0123d\n", -420);
 	 	 printf ("---------------------------\n");
 	printf("x1 = %d\nx2 = %d\n", x1, x2);
  // 	ft_printf("MY PRINTF:\ndigit(d) = %i\ndigit(i) = %i\nstring: %s \nchar: %c\npointer: %p\n", 666, 6,"awesome", 'c', &x);
